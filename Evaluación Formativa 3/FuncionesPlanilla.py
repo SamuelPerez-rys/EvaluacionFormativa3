ae4655de557369efe5162465632560a2b7d1d790
ManuelADMN <ma.dzm@outlook.com>
2024-06-17 17:39:58 -0400

    Ola

--- Evaluación Formativa 3/FuncionesPlanilla.py
+++ Evaluación Formativa 3/FuncionesPlanilla.py
@@ -3,6 +3,7 @@ import csv
 
 with open('ArchivoNuevo_CSV.csv','w',newline='',encoding='utf-8') as arch:
     encabezado=['Trabajador','Cargo','Sueldo Bruto','Desc. Salud','Desc. AFP','Liquido a pagar'];
+    matrizDatos=input("Ingrese ")
     archivo_csv=csv.writer(arch);
     archivo_csv.writerow(encabezado);
-    archivo_csv.writerow(maatrizDatos);
+    archivo_csv.writerow(matrizDatos);
