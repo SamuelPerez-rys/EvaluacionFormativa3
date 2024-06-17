4a430f94c5360309dcbbe6dda0809c5c9f83fd52
ManuelADMN <ma.dzm@outlook.com>
2024-06-17 17:18:32 -0400

    Add files via upload

---
+++ Evaluación Formativa 3/Evaluacion formativa 3.py
@@ -0,0 +1,13 @@
+###Empresa DUOC
+import FuncionesPlanilla as func
+
+print("\nBienvenido a DUOC Empresa\n\nElija una opción:  ");
+print("1.- Registrar Trabajador");
+print("2.- Lista de trabajadores");
+print("3.- Imprimir planilla de sueldos");
+print("4.- Salir del programa");
+
+menuDecision=int(input("\nIngrese una opción:  "));
+
+if (menuDecision==1):
+    func
