5b8c1277c9a2da0a7e5dcc8a9007d36ff5757969
ManuelADMN <ma.dzm@outlook.com>
2024-06-17 17:50:14 -0400

    RegistroTrabajadores

--- Evaluación Formativa 3/FuncionesPlanilla.py
+++ Evaluación Formativa 3/FuncionesPlanilla.py
@@ -1,6 +1,23 @@
 ###Funciones Planilla
 import csv
 
+def registrarTrabajador():
+    try: 
+        nombre=input("Ingrese el nombre y el apellido del trabajador");
+    except ValueError:
+        print("Nombre y apellido son obligatorios");
+    else:
+        try: 
+            cargo=input("Ingrese el cargo del trabajador");
+        except ValueError:
+            print("Ingresar el cargo es obligatorio");
+        else:    
+
+
+
+
+
+
 with open('ArchivoNuevo_CSV.csv','w',newline='',encoding='utf-8') as arch:
     encabezado=['Trabajador','Cargo','Sueldo Bruto','Desc. Salud','Desc. AFP','Liquido a pagar'];
     matrizDatos=input("Ingrese ")
